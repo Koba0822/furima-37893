6600de41796532a2611e66619e944a5a2cf205a2
Koba0822 <o93149314@icloud.com>
2022-09-15 01:19:10 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,4 +18,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
+
+private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
