611e197a833027ee3c6041525a5806610bcabd7a
Koba0822 <o93149314@icloud.com>
2022-09-14 19:28:37 +0900

    Revert "Revert "ユーザー管理機能実装""
    This reverts commit 36a25f66fc96511e1ecee389e22e7f0a54d1172f.

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+
+with_options presence: true do
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers'}
+  validates :nickname, presence: true
+  validates :last_name,  presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana , presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :user_birth_date, presence: true
+end
+
+  has_many :items
+  has_many :buy_items
+end
