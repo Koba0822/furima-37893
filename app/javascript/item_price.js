6b2f6c24107916726e1faa12cd8ddb61681ee1c1
Koba0822 <o93149314@icloud.com>
2022-09-15 16:26:02 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,15 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  const ProfitDom = document.getElementById("profit");
-  // 入力した金額をもとに販売手数料を計算する処理
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  // 出品価格から販売手数料を引く処理
-  ProfitDom.innerHTML = (Math.floor(inputValue - value_result));
+window.addEventListener("DOMContentLoaded", () => {
+  if ( document.getElementById('item-price')!= null) {
+    // 読み込まれたページに'item-price'というid名の要素があれば発火
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    priceInput.addEventListener("input", () => {
+      const inputValue = document.getElementById("item-price").value;
+        let tax = Math.floor(inputValue * 0.1).toLocaleString(); 
+        addTaxDom.innerHTML = tax ;
+        profitDom.innerHTML = Math.ceil(inputValue * 0.9).toLocaleString();
+    })
+  }
 });
