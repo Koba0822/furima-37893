51f55518ba1b9d020c0fd6059f68a6d7d00cebc9
Koba0822 <o93149314@icloud.com>
2022-09-15 19:53:51 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,22 +7,13 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
   end
 
   def new
-    @item = Item.new
   end
 
 end
 
 private
 
-  def item_params
-    params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :prefecture_id, :item_status_id, :item_shipping_cost_id, :item_prefecture_id, :item_shipping_date_id, :item_price).merge(user_id: current_user.id)
-  end
+
