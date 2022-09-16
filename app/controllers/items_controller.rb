dab24a3c5bc2aacf747265b8e02438c9d03d6cda
Koba0822 <o93149314@icloud.com>
2022-09-16 16:08:20 +0900

    詳細ページ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,35 @@
 class ItemsController < ApplicationController
+  # ログインしていないユーザーはログインページに促す
+  before_action :authenticate_user!, except: [:index, :show]
 
-  before_action :authenticate_user!, except: [:index]
+  # 重複処理をまとめる
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.includes(:user).order('created_at DESC')
-  end
-
-  def create
+    @items =Item.all
   end
 
   def new
   end
 
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_info, :item_catetegory_id, :item_status_id, :item_shipping_cost_id, :prefecture_id, :item_shipping_date_id, :item_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
 end
-
-private
-
-
