bda2647cc8b2dfbceffd4ebe72deda6304ea8bdf
Koba0822 <o93149314@icloud.com>
2022-09-29 20:30:45 +0900

    FactoryBot修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,20 +1,15 @@
 class OrderForm < ApplicationRecord
   include ActiveModel::Model
-  # order_idは、保存されたタイミングで生成されるため、attr_accessorにおいて不要なカラムとなる（書くと蛇足なのでエラー）
   attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
 
-  # 4行目と同じくこのタイミングでは生成前なので「validates :order_id」は不要
   with_options presence: true do
-    # orderモデルのバリデーション
     validates :user_id
     validates :item_id
-    # paymentモデルのバリデーション
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-    # トークンのバリデーション
     validates :token
   end
 
